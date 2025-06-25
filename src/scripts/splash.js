eadbef24440d91036c529c8b3d1787a2defd294d
Vadym <vadim.rolex.2005@gmail.com>
2025-06-25 19:12:25 +0300

    Main screen is done. This window is for testing only.

---
+++ src/scripts/splash.js
@@ -0,0 +1,15 @@
+setTimeout(() => {
+  const splashScreen = document.getElementById('splashScreen');
+  const mainMenu = document.getElementById('mainMenu');
+  const backgroundVideo = document.getElementById('backgroundVideo');
+  const backgroundMusic = document.getElementById('backgroundMusic');
+
+  splashScreen.classList.add('fade-out');
+
+  setTimeout(() => {
+    splashScreen.style.display = 'none';
+    backgroundVideo.classList.add('show');
+    mainMenu.style.display = 'flex';
+    backgroundMusic.play();
+  }, 1000);
+}, 4000);
